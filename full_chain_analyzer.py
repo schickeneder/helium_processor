b4ff36641fa0909bb18ca2132fb4556b3b7db9ee
ps <schickeneder@gmail.com>
2024-03-18 21:35:53 -0600

    refactored with cupy for denylist, compute percentile distances

--- full_chain_analyzer.py
+++ full_chain_analyzer.py
@@ -8,6 +8,9 @@ import h3
 import matplotlib.pyplot as plt
 import time
 import multiprocessing
+from itertools import repeat
+from numba import jit
+import cupy as cp
 
 
 from pickle_maker import find_csv_files
@@ -83,6 +86,7 @@ def get_pairwise_distances(h3dict, locations, h3dist_limit=100):
     return node_dist_pairs_dict
 
 # single location version of the function, for use with multi below
+#@jit(forceobj=True, looplift=True)
 def get_pairwise_distances_multi_helper(h3dict, row):
     #global h3dict
     global h3dist_limit
@@ -103,16 +107,62 @@ def get_pairwise_distances_multi_helper(h3dict, row):
                     node_dist_pairs_dict_single[origin_node].append(h3.h3_distance(origin_loc, h3cell))
 
     return node_dist_pairs_dict_single
+
 def get_pairwise_distances_multi(h3dict,locations):
     node_dist_pairs_dict = {}
 
     with multiprocessing.Pool(processes=32) as pool:
         results = pool.starmap(get_pairwise_distances_multi_helper, [(h3dict,location) for location in locations])
+        #results = pool.starmap(get_pairwise_distances_multi_helper, zip(repeat(h3dict),locations))
+
+        # zip(repeat(origin_node), h3dict[h3cell]
 
     for item in results:
         node_dist_pairs_dict.update(item)
 
     return node_dist_pairs_dict
+
+
+def get_pairwise_distances_multi_helper2b(h3dict, row, h3cell):
+    if h3cell in h3dict:
+        for remote_node in h3dict[h3cell]:
+            if remote_node < row[0]:
+                return h3.h3_distance(row[3],h3cell)
+def get_pairwise_distances_multi_helper2(origin_loc, remote_loc):
+
+    if remote_loc < origin_loc:
+        try:
+            result = h3.h3_distance(origin_loc,remote_loc)
+            if result < h3dist_limit:
+                return result
+        except:
+            pass
+
+    return
+
+
+def get_coords_from_h3cells(locations):
+    coords = []
+    for row in locations:
+        coords.append(h3.h3_to_geo(row[3]))
+
+# trying this a different way..
+# [gateway], [timestamp], [block], [location], [owner], [gain?], [elevation?]
+# assume locations is sorted based on gateway
+
+def get_pairwise_distances_multi2(locations):
+    all_dist = []
+
+    # for origin_node in locations:
+    #     for remote_node in locations:
+
+    with multiprocessing.Pool(processes=32) as pool:
+        for location in locations:
+            results = pool.starmap(get_pairwise_distances_multi_helper2, [(location[3],item[3]) for item in locations])
+            all_dist += results
+
+    return all_dist
+
 
 # creates a dictionary of h3cells based on a location list, where location list is:
 # [gateway], [timestamp], [block], [location], [owner], [gain?], [elevation?]
@@ -276,25 +326,70 @@ def plot_hist(freq_dist_data):
 # make sure input_list and denylist are both either shrink or regular
 def denylist_filter_distances_list(input_list, denylist):
     new_list = []
+    length = len(input_list)
+    count = 0
 
     for element in input_list:
+        count += 1
+        if not count % 100000:
+            print(f"Count {count} of {length}")
         txgateway = element[-2]
         wgateway = element[-1]
 
         if txgateway in denylist or wgateway in denylist:
+            #print(f"removing element")
             continue
+
         else:
             new_list.append(element)
 
     return new_list
 
+def denylist_filter_distances_list_v2(input_list, denylist):
+    new_list = []
+    length = len(input_list)
+    count = 0
+
+    distances = [row[2] for row in input_list]
+    gateways = [[row[-2],row[-1]] for row in input_list]
+    print("Done extracting gateways")
+    print(f"{gateways[:10]}")
+
+    distances_cp = cp.array(distances)
+    array = cp.array(gateways)
+    denylist_cp = cp.array(denylist)
+    print("created cp.array, distances_cp and denylist_cp")
+    print(f"{array[:10,1]}")
+    print(f"{distances_cp[:10]}")
+    print(f"{denylist_cp[:10]}")
+
+    is_in_denylist = cp.any(cp.isin(array,denylist_cp), axis=1)
+
+    print("created is_in_denylist")
+    print(f"{is_in_denylist}")
+    print(f"True elements {cp.sum(is_in_denylist)} and total elements {len(is_in_denylist)}")
+
+    return list(distances_cp[~is_in_denylist])
+
 def update_h3dict(new_dict):
     global h3dict
     h3dict = new_dict
 
+# prints and returns percentiles for a list of values
+def get_percentiles(input_values):
+    # convert it!
+    values = cp.array(input_values)
+    percentiles = [25,50,75,90,99]
+    results = []
+    for percentile in percentiles:
+        result = int(cp.percentile(values,percentile))
+        results.append(result)
 
-block_limit = 100000
-h3dist_limit = 30
+    print(f"{results}/{percentiles} are results/percentiles for {len(input_values)} rows in denylist-filtered distances list")
+
+
+block_limit = 100000 #1837239
+h3dist_limit = 5
 h3dict = {}
 
 if __name__ == "__main__":
@@ -327,36 +422,60 @@ if __name__ == "__main__":
         # else: # just process but don't write
         #     node_dict = build_node_dict(csvgz_file)
 
-    # distances_filepath = transaction_filepath + "\shrink_distances_10000.pickle"
-    # with open(distances_filepath, 'rb') as file:
-    #             distances = pickle.load(file)
-    #
+    # histogram or distance percentiles
+
+    distances_filepath = transaction_filepath + "\shrink_distances_nz.pickle"
+    with open(distances_filepath, 'rb') as file:
+                distances = pickle.load(file)
+
+    with open(r'D:\denylists\shrink_unique_denylist.pickle', 'rb') as file:
+                shrink_denylist = pickle.load(file)
     # with open(r'D:\blockchain-etl-export\transactions\shrink_gateway_mapping.pickle', 'rb') as file:
     #             shrink_denylist = pickle.load(file)
+
+    # print(f"First 10 of distances used: {distances[:10]}")
+    # #print(shrink_denylist)
+    # print(f"First 10 of shrink_denylist used: {shrink_denylist[:10]}")
+
+
+    # row[2] of distances contains the distances between nodes for a witness event
+    # res, pers = get_percentiles([distance[2] for distance in distances])
+    # print(f"{res}/{pers} are results/percentiles for {len(distances)} rows in full distances list")
+
+    print("extracted pickl, starting denylist filter")
+
+    new_distances = list(denylist_filter_distances_list_v2(distances, shrink_denylist))
+    print("Done with denylist_filter")
+    #new_distances = [distance[2] for distance in distances[not_in_deny_list]]
+    print("Done extracting just the distance columns")
+    get_percentiles(new_distances)
+
+    #results = get_witness_h3distance_frequency_distribution(new_distances)
+
+    #print(f"Average count {np.mean(np.array(list(results.values())))}, stdev {np.std(np.array(list(results.values)))}")
+    #plot_hist(results)
+
+
+
+    # #----------pairwise distances----------------
+    # start_time = time.time()
     #
-    # new_distances = denylist_filter_distances_list(distances, shrink_denylist)
+    # locations_filepath = transaction_filepath + "\locations.pickle"
+    # with open(locations_filepath, 'rb') as file:
+    #             locations = pickle.load(file)
     #
-    # results = get_witness_h3distance_frequency_distribution(new_distances)
+    # #locations.sort(key=lambda x: x[0])
     #
-    # #print(f"Average count {np.mean(np.array(list(results.values())))}, stdev {np.std(np.array(list(results.values)))}")
-    # plot_hist(results)
-
-
-    start_time = time.time()
-
-    locations_filepath = transaction_filepath + "\locations.pickle"
-    with open(locations_filepath, 'rb') as file:
-                locations = pickle.load(file)
-
-    h3dict = populate_h3cells(locations, block_limit=block_limit)
-    update_h3dict(h3dict)
-
-    #results = get_pairwise_distances(h3dict,locations, h3dist_limit)
-    results = get_pairwise_distances_multi(h3dict, locations)
-
-    print(f"Elapsed time: {(time.time()-start_time):.2f}s with block_limit {block_limit} and h3dist_limit {h3dist_limit}")
-    # print(len(h3cells))
-
-    # this is REALLY slow.. can we fix it with matrix multiplication? numpy?
+    # h3dict = populate_h3cells(locations, block_limit=block_limit)
+    # # print(locations[:10])
+    # # print(locations[-10:])
+    # # print(len(h3dict))
+    # update_h3dict(h3dict)
+    # #results = get_pairwise_distances(h3dict,locations, h3dist_limit)
+    # results = get_pairwise_distances_multi(h3dict, locations)
+    # #results = get_pairwise_distances_multi2(locations)
+    # print(f"Elapsed time: {(time.time()-start_time):.2f}s with block_limit {block_limit} and h3dist_limit {h3dist_limit}")
+    # # print(len(h3cells))
+    # # this is REALLY slow.. can we fix it with matrix multiplication? numpy?
 
     code.interact(local=locals())
